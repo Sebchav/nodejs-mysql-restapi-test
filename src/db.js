ff6fc5eaea742055f3b0f77b6444f0d1ccf238e6
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2022-10-19 19:50:06 -0500

    Cambios

--- src/db.js
+++ src/db.js
@@ -3,14 +3,12 @@ import {
     DB_HOST,
     DB_PORT,
     DB_DATABASE,
-    DB_USER,
-    DB_PASSWORD
+    DB_USER
 } from "./config.js"
 
 export  const pool = createPool({
     host: DB_HOST,
     user: DB_USER,
     port: DB_PORT,
-    database: DB_DATABASE,
-    password: DB_PASSWORD
+    database: DB_DATABASE
 })
